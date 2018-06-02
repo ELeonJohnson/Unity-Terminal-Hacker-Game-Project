b500f6deca31d21ac8d85e40131958e149908ab4
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-06-02 12:46:20 -0400

    Starting Terminal Hacker Game Unity Project

---
+++ Assets/Hacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ShowMainMenu("Enoch");  
+    }
+
+    void ShowMainMenu(string userInput)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello " + userInput);
+        Terminal.WriteLine("Pick a type of player.");
+        Terminal.WriteLine("Press 1: All Stars");
+        Terminal.WriteLine("Press 2: Stars");
+        Terminal.WriteLine("Press 3: Rookies");
+        Terminal.WriteLine("Enter Your Selection Below: ");
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
