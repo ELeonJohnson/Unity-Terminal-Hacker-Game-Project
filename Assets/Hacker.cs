019199723167147313d0a571fa78b454c76ff326
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-06-03 02:12:16 -0400

    added enums for game state & refactored some code

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
+    // Game State
     int level;
-    string gamerName;
+    enum Screen { MainMenu, Password, Win};
+    Screen currentScreen;
+    string password;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        ShowMainMenu("Player 1");
+        ShowMainMenu();
         OnUserInput("");
        
     }
 
-    void ShowMainMenu(string greeting)
+    void ShowMainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-        Terminal.WriteLine("Hello " + greeting);
         Terminal.WriteLine("Pick a type of player.");
         Terminal.WriteLine("Press 1: All Stars");
         Terminal.WriteLine("Press 2: Stars");
@@ -29,38 +32,63 @@ public class Hacker : MonoBehaviour {
 
     void OnUserInput(string input)
     {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMethod(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+
+    private void RunMainMethod(string input)
+    {
         if (input == "1")
         {
             level = 1;
+            password = "allstars";
             StartGame();
         }
         else if (input == "2")
         {
             level = 2;
+            password = "stars";
             StartGame();
         }
         else if (input == "3")
         {
             level = 3;
+            password = "rookies";
             StartGame();
         }
-        else if (input == "24" || input == "8")
-        {
-            Terminal.WriteLine("Which kobe do you perfer, 8 or 24?");
-        }
-        else if (input == "menu")
-        {
-            ShowMainMenu("Enoch");
-        }
-        else 
+        else
         {
             Terminal.WriteLine("Please choose a correct level.");
-        }    
+        }
     }
 
     void StartGame()
     {
+        currentScreen = Screen.Password;
         Terminal.WriteLine("You have chosen the level " + level);
+        Terminal.WriteLine("Please enter your password:");
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("Great Job!");
+        }
+        else
+        {
+            Terminal.WriteLine("Wrong password, try again.");
+        }
     }
 
 
