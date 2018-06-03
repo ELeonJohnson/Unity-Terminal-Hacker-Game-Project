d8dc58f7ac61729e726e4fb6dad033900442219d
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-06-02 23:23:12 -0400

    more practice with unity...added conditional statement and member variables

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,28 +4,67 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
+    int level;
+    string gamerName;
+
 	// Use this for initialization
-	void Start () {
-        ShowMainMenu("Enoch");  
+	void Start ()
+    {
+        ShowMainMenu("Player 1");
+        OnUserInput("");
+       
     }
 
-    void ShowMainMenu(string userInput)
+    void ShowMainMenu(string greeting)
     {
         Terminal.ClearScreen();
-        Terminal.WriteLine("Hello " + userInput);
+        Terminal.WriteLine("Hello " + greeting);
         Terminal.WriteLine("Pick a type of player.");
         Terminal.WriteLine("Press 1: All Stars");
         Terminal.WriteLine("Press 2: Stars");
         Terminal.WriteLine("Press 3: Rookies");
-        Terminal.WriteLine("Enter Your Selection Below: ");
+        Terminal.WriteLine("Enter Your Selection Below:");
+       
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else if (input == "24" || input == "8")
+        {
+            Terminal.WriteLine("Which kobe do you perfer, 8 or 24?");
+        }
+        else if (input == "menu")
+        {
+            ShowMainMenu("Enoch");
+        }
+        else 
+        {
+            Terminal.WriteLine("Please choose a correct level.");
+        }    
+    }
+
+    void StartGame()
+    {
+        Terminal.WriteLine("You have chosen the level " + level);
     }
 
 
 
 
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
