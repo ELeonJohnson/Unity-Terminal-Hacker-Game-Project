f3daccb0dd88e9a78b01d3630a40c411d20dc319
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-06-03 14:57:47 -0400

    added arrys, random genarator, win screen & switch

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+    //Game Configuration Data
+    string[] levelOnePasswords = { "steph", "lebron", "anthony", "james", "kyrie", "russel" };
+    string[] levelTwoPasswords = { "draymond", "kevin", "rondo", "chris", "gordon","carmelo" };
+    string[] levelThreePasswords = { "donovan", "jayson", "ben", "kyle", "dennis", "lauri" };
+
 
     // Game State
     int level;
     enum Screen { MainMenu, Password, Win};
     Screen currentScreen;
     string password;
+    const string menuHint = "Enter 'menu' to return to menu screen";
 
     // Use this for initialization
     void Start ()
@@ -23,9 +29,9 @@ public class Hacker : MonoBehaviour {
         currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("Pick a type of player.");
-        Terminal.WriteLine("Press 1: All Stars");
-        Terminal.WriteLine("Press 2: Stars");
-        Terminal.WriteLine("Press 3: Rookies");
+        Terminal.WriteLine("Enter 1: All Stars");
+        Terminal.WriteLine("Enter 2: Stars");
+        Terminal.WriteLine("Enter 3: Rookies");
         Terminal.WriteLine("Enter Your Selection Below:");
        
     }
@@ -46,53 +52,88 @@ public class Hacker : MonoBehaviour {
         }
     }
 
-    private void RunMainMethod(string input)
+    void RunMainMethod(string input)
     {
-        if (input == "1")
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+
+        if (isValidLevelNumber)
         {
-            level = 1;
-            password = "allstars";
-            StartGame();
-        }
-        else if (input == "2")
-        {
-            level = 2;
-            password = "stars";
-            StartGame();
-        }
-        else if (input == "3")
-        {
-            level = 3;
-            password = "rookies";
-            StartGame();
+            level = int.Parse(input);
+            AskForPassword();
         }
         else
         {
-            Terminal.WriteLine("Please choose a correct level.");
+            ShowMainMenu();
         }
     }
 
-    void StartGame()
+    void AskForPassword()
     {
         currentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen the level " + level);
-        Terminal.WriteLine("Please enter your password:");
+        Terminal.ClearScreen();
+        SetRandomPassword();
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                int randomLevelOneIndex = Random.Range(0, levelOnePasswords.Length);
+                password = levelOnePasswords[randomLevelOneIndex];
+                break;
+            case 2:
+                int randomLevelTwoIndex = Random.Range(0, levelTwoPasswords.Length);
+                password = levelTwoPasswords[randomLevelTwoIndex];
+                break;
+            case 3:
+                int randomLevelThreeIndex = Random.Range(0, levelThreePasswords.Length);
+                password = levelThreePasswords[randomLevelThreeIndex];
+                break;
+            default:
+                Debug.LogError("Invalid Level Number");
+                break;
+        }
+        Terminal.WriteLine("Enter the player's name: " + password.Anagram());
+        Terminal.WriteLine(menuHint);
     }
 
     void CheckPassword(string input)
     {
         if (input == password)
         {
-            Terminal.WriteLine("Great Job!");
+            DisplayWinScreen();
         }
         else
         {
-            Terminal.WriteLine("Wrong password, try again.");
+            AskForPassword();
         }
     }
 
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
 
-
-
-
+    void ShowLevelReward()
+    {
+        switch(level)
+        {
+            case 1:
+                Terminal.WriteLine("You guessed a All Star");
+                Terminal.WriteLine(menuHint);
+                break;
+            case 2:
+                Terminal.WriteLine("You guessed a Star");
+                Terminal.WriteLine(menuHint);
+                break;
+            case 3:
+                Terminal.WriteLine("You guessed a Rookie");
+                Terminal.WriteLine(menuHint);
+                break;
+        }
+        
+    }
 }
